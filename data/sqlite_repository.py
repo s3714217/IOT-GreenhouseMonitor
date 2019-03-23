767e95ebd5f32009b47fec3432e2201d8a30972b
Daniel Majoinen <github@d.majoinen.com>
2019-03-23 14:00:06 +1100

    implement basic sqlite repository

---
+++ data/sqlite_repository.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+'''
+Generic SQLite repository
+'''
+class SqliteRepository():
+
+    seperator = ", "
+
+    insert_sql = "INSERT INTO %s (%s) VALUES (%s)"
+
+    def __init__(self, database):
+        self.__database = database
+
+    '''
+    Executes the supplied SQL query
+    '''
+    def execute(self, sql):
+        connection = sqlite3.connect(self.__database)
+        try:
+            return connection.execute(sql)
+        finally:
+            connection.commit()
+            connection.close()
+
+    '''
+    Insert an entry into specified table with supplied items dictionary
+    '''
+    def insert(self, table, items):
+        # Split out the dictionary into separate lists
+        keys = []
+        values = []
+        for key, value in items.items():
+            keys.append(key)
+            values.append(str(value))
+        # Join each list into single strings
+        keys_joined = self.seperator.join(keys)
+        values_joined = self.seperator.join(values)
+        # Insert into database
+        return self.execute(self.insert_sql % (table, keys_joined, values_joined))
