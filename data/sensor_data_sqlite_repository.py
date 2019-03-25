622f74ac2af1bf9ec333c93edd2ecff2b5946f3b
Daniel Majoinen <github@d.majoinen.com>
2019-03-25 20:15:37 +1100

    add ability to get out of range entries

--- data/sensor_data_sqlite_repository.py
+++ data/sensor_data_sqlite_repository.py
@@ -1,12 +1,29 @@
+import logging
 import os
+from datetime import datetime
 from data.sqlite_repository import SqliteRepository
+from models.sensor_log import SensorLog
 
 '''
 SQLite repository to handle persisting sensor data
 '''
 class SensorDataSqliteRepository(SqliteRepository):
 
-    def __init__(self):
+    TEMPERATURE_INDEX = 0
+    HUMIDITY_INDEX = 1
+    TIMESTAMP_INDEX = 2
+    DATE_FORMAT = "%Y-%m-%d"
+
+    SELECT_DAYS_LOGS_SQL = "SELECT temperature, humidity, timestamp FROM SensorLog \
+        WHERE timestamp >= DATE(:date) AND timestamp < DATE(:date, '+1 day')"
+
+    COUNT_OUT_OF_RANGE_LOGS_SQL = "SELECT COUNT(*) FROM SensorLog \
+        WHERE timestamp BETWEEN DATE(:date) AND DATE(:date, '+1 day') \
+        AND (temperature NOT BETWEEN :min_temperature AND :max_temperature \
+        OR humidity NOT BETWEEN :min_humidity AND :max_humidity)"
+
+    def __init__(self, config):
+        self.__config = config
         database = os.getcwd()+"/resources/sensor_data.db"
         super().__init__(database)
 
@@ -14,9 +31,42 @@ class SensorDataSqliteRepository(SqliteRepository):
     Insert an entry into the SensorLog table
     '''
     def insert_sensor_log(self, temperature, humidity):
+        logging.debug("Inserting SensorLog Entry - TEMPERATURE: %s, HUMIDITY: %s" \
+            % (temperature, humidity))
+
         table = "SensorLog"
         items = {
             "TEMPERATURE": temperature,
             "HUMIDITY": humidity
         }
+
         super().insert(table, items)
+
+    '''
+    Count the total entries outside the configured ranges for any given day
+    '''
+    def count_out_of_range_logs(self, date):
+        result = super().execute(self.COUNT_OUT_OF_RANGE_LOGS_SQL, {
+            "date": date.strftime(self.DATE_FORMAT),
+            "min_temperature": self.__config.get_min_temperature(),
+            "max_temperature": self.__config.get_max_temperature(),
+            "min_humidity": self.__config.get_min_humidity(),
+            "max_humidity": self.__config.get_max_humidity()
+        }).fetchone()
+        logging.debug("Counted %d entries outside ranges" % result[0])
+        return result[0]
+
+    '''
+    Select all sensor logs for the supplied date
+    '''
+    def select_days_logs(self, date):
+        results = super().execute(self.SELECT_DAYS_LOGS_SQL, \
+            { "date": date.strftime(self.DATE_FORMAT) })
+
+        logs = []
+        for result in results:
+            logs.append(SensorLog(
+                result[self.TEMPERATURE_INDEX], 
+                result[self.HUMIDITY_INDEX], 
+                result[self.TIMESTAMP_INDEX]))
+        return logs
--- monitorAndNotify.py
+++ monitorAndNotify.py
@@ -1,21 +1,64 @@
+import logging
 import os
+from datetime import datetime
+from config import Config
 from data.sensor_data_sqlite_repository import SensorDataSqliteRepository
 from data.database_initialiser import DatabaseInitialiser
 from virtual_sense_hat import VirtualSenseHat
 
 class MonitorAndNotify:
 
-    def __init__(self):
-        DatabaseInitialiser().init_sensor_data()
-        self.__sensor_data_repository = SensorDataSqliteRepository()
+    def __init__(self, sensor_data_repository):
+        self.__repository = sensor_data_repository
 
+    '''
+    Logs the current readings into the database and notifies via pushbullet if
+    a reading is out of the ranges supplied in the config
+    '''
     def monitor_sensor_data(self):
         # Get readings from SenseHat
         sense = VirtualSenseHat.getSenseHat()
         temperature = sense.get_temperature()
         humidity = sense.get_humidity()
         # Store in database
-        self.__sensor_data_repository.insert_sensor_log(temperature, humidity)
-        # TODO: Send PushBullet notification if outside range and would not have sent already
+        self.__repository.insert_sensor_log(temperature, humidity)
 
-MonitorAndNotify().monitor_sensor_data()
+    '''
+    Sends a notification if outside the ranges supplied in the config
+    '''
+    def notify_if_required(self):
+        notify = self.notification_required()
+        if notify is True:
+            logging.info("Notification required")
+            self.notify_outside_range()
+        else:
+            logging.info("Notification not required")
+
+    '''
+    Determines if the reading was outside the ranges supplied in the config
+    '''
+    def notification_required(self):
+        count = self.__repository.count_out_of_range_logs(datetime.now())
+        return count == 1
+
+    '''
+    Will notify via pushbullet because a reading was out of the ranges supplied
+    in the config. This will only occur once per day
+    '''
+    def notify_outside_range(self):
+        pass # TODO: Send PushBullet notification
+
+if __name__ == "__main__":
+    logging.basicConfig(level = logging.DEBUG)
+
+    # Initialise database
+    DatabaseInitialiser().init_sensor_data()
+
+    # Instantiate dependencies
+    config = Config()
+    repository = SensorDataSqliteRepository(config.get_sensor_config())
+
+    # Log sensor data and notify if required!
+    monitor = MonitorAndNotify(repository)
+    monitor.monitor_sensor_data()
+    monitor.notify_if_required()
