26be75a9499da79d113a0ea8107649b5939ff738
Daniel Majoinen <github@d.majoinen.com>
2019-03-25 21:24:13 +1100

    add pushbullet integration

---
+++ pushbullet/pushbullet.py
@@ -0,0 +1,29 @@
+import json
+import logging
+import requests
+
+class PushBullet:
+
+    def __init__(self, token):
+        self.__token = token
+
+    '''
+    Send a notification via PushBullet with a title and body
+    '''
+    def send_note(self, title, body):
+        logging.debug("Sending notification - Title: %s" % title)
+        logging.debug("Sending notification - Body: %s" % body)
+
+        data = { "type": "note", "title": title, "body": body }
+        response = requests.post("https://api.pushbullet.com/v2/pushes", 
+            data = json.dumps(data),
+            headers = { 
+                "Authorization": "Bearer " + self.__token, 
+                "Content-Type": "application/json" 
+            })
+
+        if(response.status_code != 200):
+            logging.error("Failed to send PushBullet notification")
+            raise Exception()
+
+        logging.info("Notification sent.")
