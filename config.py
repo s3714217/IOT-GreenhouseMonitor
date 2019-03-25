3d51009d336f82bbe6f1aee6103cb6899d4d50c6
Daniel Majoinen <github@d.majoinen.com>
2019-03-25 20:51:08 +1100

    add pushbullet token

--- config.py
+++ config.py
@@ -9,11 +9,22 @@ class Config:
         with open(os.getcwd()+"/config.json", "r") as config_file:
             # Create required model classes
             config = json.load(config_file)
+            # Sensor config
             self.__sensor_config = SensorConfig(
                 config["min_temperature"], 
                 config["max_temperature"],
                 config["min_humidity"],
                 config["max_humidity"])
+            # PushBullet config
+            if "pushbullet_token" in config:
+                 logging.error("PushBullet token is missing \
+                      - Add pushbullet_token entry in config.json")
+                self.__pushbullet_token = config["pushbullet_token"]
+            else:
+                self.__pushbullet_token = None
 
     def get_sensor_config(self):
         return self.__sensor_config
+
+    def get_pushbullet_token(self):
+        return self.__pushbullet_token
