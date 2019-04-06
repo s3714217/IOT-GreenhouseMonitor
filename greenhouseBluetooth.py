2a04e89e21733467f536c8196a372e35565a7f32
Daniel Majoinen <github@d.majoinen.com>
2019-04-07 09:07:34 +1000

    fix dependencies

--- greenhouseBluetooth.py
+++ greenhouseBluetooth.py
@@ -1,18 +1,21 @@
+#!/usr/bin/env python3
 import logging
-from config import Config
-from pushbullet.pushbullet import PushBullet
-import subprocess as sp
 import os
+import subprocess as sp
+from config import Config
+from notifications.notification import Notification
 
 class GreenhouseBluetooth:
 
-    def __init__(self, pushbullet):
-        self.__pushbullet = pushbullet
+    def __init__(self, sensor_data_repository, pushbullet_token):
+        self.__notification = None if pushbullet_token is None else \
+            Notification(sensor_data_repository, pushbullet_token)
 
     def get_paired_bluetooth_devices(self):
-       p = sp.Popen(["bt-device","--list"], stding = sp.PIPE, stdout = sp.PIPE, close-fds = True) 
-       (stdout, stdin) = (p.stdout, p.stdin) 
+        p = sp.Popen(["bt-device","--list"], stding = sp.PIPE, stdout = sp.PIPE, close-fds = True) 
+        (stdout, stdin) = (p.stdout, p.stdin) 
         data = stdout.readlines()
+        logging.debug(data)
         str2 = []
 
       for str1 in data:
@@ -33,17 +36,21 @@ class GreenhouseBluetooth:
            if (str1 == mac.encoded()):#the above mac_address is still encoded so I encoded the nearby address to comparing
               currently_connected_device.append(mac)#Identified connected device
             else:  # TODO: Send notification to devices we have not yet sent a notification to today
-         
         pass
 
 if __name__ == "__main__":
     logging.basicConfig(level = logging.DEBUG)
 
+    # Initialise database
+    DatabaseInitialiser().init_sensor_data()
+
     # Instantiate dependencies
     config = Config()
+    sensor_config = config.get_sensor_config()
     pushbullet_token = config.get_pushbullet_token()
-    pushbullet = PushBullet(pushbullet_token)
+    repository = SensorDataSqliteRepository(sensor_config)
 
-    bluetooth = GreenhouseBluetooth(pushbullet)
+    # Check for connected bluetooth devices and notify
+    bluetooth = GreenhouseBluetooth(repository, pushbullet_token)
     paired_devices = bluetooth.get_paired_bluetooth_devices()
     bluetooth.notify_if_required(paired_devices)
