c17d9242f6a3307ff98e0f7e0e66325fb3b013fb
daniel <daniel@majoinen.com>
2019-04-06 13:44:47 +1100

    refactor notifications to allow devices

--- monitorAndNotify.py
+++ monitorAndNotify.py
@@ -1,61 +1,53 @@
 import logging
-import os
-from datetime import datetime
 from config import Config
-from data.sensor_data_sqlite_repository import SensorDataSqliteRepository
 from data.database_initialiser import DatabaseInitialiser
-from pushbullet.pushbullet import PushBullet
+from data.sensor_data_sqlite_repository import SensorDataSqliteRepository
+from helpers.readings_validator import ReadingsValidator
+from models.sensor_log import SensorLog
+from notifications.notification import Notification
 from virtual_sense_hat import VirtualSenseHat
 
 class MonitorAndNotify:
 
-    def __init__(self, sensor_data_repository, pushbullet_token = None):
+    def __init__(self, sensor_data_repository, readings_validator, pushbullet_token = None):
         self.__repository = sensor_data_repository
-        self.__pushbullet_token = pushbullet_token
+        self.__readings_validator = readings_validator
+        self.__notification = None if pushbullet_token is None else \
+            Notification(self.__repository, pushbullet_token)
 
     '''
-    Logs the current readings into the database and notifies via pushbullet if
-    a reading is out of the ranges supplied in the config
+    Logs the current readings into the database
     '''
     def monitor_sensor_data(self):
         # Get readings from SenseHat
         sense = VirtualSenseHat.getSenseHat()
         temperature = sense.get_temperature()
         humidity = sense.get_humidity()
+        sensor_log = SensorLog(temperature, humidity)
         # Store in database
-        self.__repository.insert_sensor_log(temperature, humidity)
+        self.__repository.insert_sensor_log(sensor_log)
+        return sensor_log
 
     '''
     Sends a notification if outside the ranges supplied in the config
     '''
-    def notify_if_required(self):
-        if self.__pushbullet_token is None:
+    def notify_if_required(self, sensor_log):
+        if self.__notification is None:
             return
 
-        notify = self.notification_required()
-        if notify is True:
+        # Validate readings were outside ranges
+        results = self.__readings_validator.validate_sensor_log(sensor_log)
+        if len(results) is 0:
+            logging.info("Notification not required")
+            return
+        
+        # Ensure a notification has not already been sent
+        previously_sent = self.__notification.previously_sent()
+        if previously_sent is False:
             logging.info("Notification required")
-            self.notify_outside_range()
+            self.__notification.notify_outside_range(results)
         else:
             logging.info("Notification not required")
-
-    '''
-    Determines if the reading was outside the ranges supplied in the config
-    '''
-    def notification_required(self):
-        date = datetime.now()
-        if (self.__repository.count_notifications_sent(date) != 0):
-            return False
-        return self.__repository.count_out_of_range_logs(date) == 1
-
-    '''
-    Will notify via pushbullet because a reading was out of the ranges supplied
-    in the config. This will only occur once per day
-    '''
-    def notify_outside_range(self):
-        pushbullet = PushBullet(self.__pushbullet_token)
-        pushbullet.send_note("Sensor Monitor", "Out of range reading!")
-        self.__repository.insert_notification_log()
 
 if __name__ == "__main__":
     logging.basicConfig(level = logging.DEBUG)
@@ -68,8 +60,9 @@ if __name__ == "__main__":
     sensor_config = config.get_sensor_config()
     pushbullet_token = config.get_pushbullet_token()
     repository = SensorDataSqliteRepository(sensor_config)
+    readings_validator = ReadingsValidator(sensor_config)
 
     # Log sensor data and notify if required!
-    monitor = MonitorAndNotify(repository, pushbullet_token)
-    monitor.monitor_sensor_data()
-    monitor.notify_if_required()
+    monitor = MonitorAndNotify(repository, readings_validator, pushbullet_token)
+    sensor_log = monitor.monitor_sensor_data()
+    monitor.notify_if_required(sensor_log)
